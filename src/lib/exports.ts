d396b794cf33f473d73ffaf0577d231d63f72afe
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-08-06 22:25:55 +0200

    [add] Added handlers object to exports file

--- src/lib/exports.ts
+++ src/lib/exports.ts
@@ -1,9 +1,12 @@
 
 import Program from "./program.js"
 import { SoybeanConfig, ZSoybeanConfig } from "./types.js"
+import * as $handlers from './events/event_handlers.js'
 
 export function Soybean(config: SoybeanConfig) {
     ZSoybeanConfig.parse(config)
     process.title = 'Soybean'
     return new Program(config)
 }
+
+export const handlers = $handlers
