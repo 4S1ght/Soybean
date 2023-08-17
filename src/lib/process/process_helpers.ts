27713ef05d0cf69ce991faf02b4279888d59c4c2
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-08-18 01:05:06 +0200

    [bug] Fix process_helpers extractCommandArgv argv array

--- src/lib/process/process_helpers.ts
+++ src/lib/process/process_helpers.ts
@@ -3,6 +3,10 @@
  * Extracts command and argv into separate strings.
  */
 export const extractCommandArgv = (command: string | string[]): [string, string[]] => {
-    if (Array.isArray(command)) return [[...command].shift()!, command]
+    if (Array.isArray(command)) {
+        const source = [...command]
+        const argv = source.splice(1, source.length - 1)
+        return [command[0], argv]
+    }
     else return [command, []]
 }
