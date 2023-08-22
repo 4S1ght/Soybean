55f9580b618ac4c2f542485db45e533d033de6a8
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-08-23 00:51:59 +0200

    [add] handlers.update

--- src/lib/events/handlers/misc.ts
+++ src/lib/events/handlers/misc.ts
@@ -13,7 +13,8 @@ import * as helpers from '../handler_helpers.js'
 /**
  * Creates a basic callback-based event handler
  */
-export function handle <Event extends E.SoybeanEvent = E.SoybeanEvent, Meta = any>(callback: E.EventCallback<Event>, meta?: Meta): E.EventHandler<Event, Meta> {
+export function handle <Event extends E.SoybeanEvent = E.SoybeanEvent, Meta = any>
+    (callback: E.EventCallback<Event>, meta?: Meta): E.EventHandler<Event, Meta> {
 
     const handler: E.EventHandler<Event, Meta> = async function(e) {
         try {
@@ -36,7 +37,9 @@ export function handle <Event extends E.SoybeanEvent = E.SoybeanEvent, Meta = an
  * Creates an event handler group. This is useful when a single event,
  * command or task should perform multiple actions in series.
  */
-export function group<Event extends E.SoybeanEvent = E.SoybeanEvent>(callbacks: E.EventHandler<Event>[]): E.EventHandler<Event> {
+export function group<Event extends E.SoybeanEvent = E.SoybeanEvent>
+    (callbacks: E.EventHandler<Event>[]): E.EventHandler<Event> {
+
     return async function(e) {
         try {
             for (let i = 0; i < callbacks.length; i++) {
@@ -49,6 +52,7 @@ export function group<Event extends E.SoybeanEvent = E.SoybeanEvent>(callbacks: 
             return error as Error
         }
     }
+
 }
 
 // ==================================================================
@@ -81,4 +85,30 @@ export function set<Event extends E.SoybeanEvent = E.SoybeanEvent>(itemName: str
         e.set(itemName, data)
         return null
     }
+}
+
+// ==================================================================
+
+/**
+ * Updates a variable on the event object inside a grouped handler.  
+ * **Note:** The updated variable must be set before updating, otherwise the value passed to the 
+ * callback will be `undefined`. This can be done with many event handlers, such as the `set` handler.
+ * ```javascript
+ * // Example
+ * handlers.group([
+ *     handlers.set('time', Date.now()),
+ *     handlers.wait(1000),
+ *     handlers.update('time', time => Date.now() - time)
+ *     handlers.handle(e => console.log(`Done in ${e.get('time')}ms`))
+ * ])
+ * ```
+ */
+export function update<Event extends E.SoybeanEvent = E.SoybeanEvent>
+    (itemName: string, callback: <Data = any>(data: Data) => Promise<any> | any): E.EventHandler<Event> {
+
+    return async function(e) {
+        await e.updateAsync(itemName, callback)
+        return null
+    }
+
 }
