9a7409adaaaee8f8c37d134aac7c157c590fa085
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-09-22 21:35:44 +0200

    [fix] Fetch handler event object issues.

--- src/lib/events/handlers/net.ts
+++ src/lib/events/handlers/net.ts
@@ -7,14 +7,11 @@ import type * as E from '../events.js'
 // Types ============================================================
 
 type FetchInput = Parameters<typeof fetch>["0"]
+type FetchResponse = Awaited<ReturnType<typeof fetch>>
 
-type FetchResponse<Event> = Awaited<ReturnType<typeof fetch>> & {
-    /** Reference to the event object. */
-    event: Event
-}
 type FetchInit<Event extends E.SoybeanEvent> = Parameters<typeof fetch>["1"] & { 
     /** Fetch event handler callback, responsible for processing the response object. */
-    cb?: (response: FetchResponse<Event>) => any | Promise<any> 
+    cb?: (response: FetchResponse, event: Event) => any | Promise<any> 
 }
 
 // Handlers =========================================================
@@ -32,9 +29,9 @@ type FetchInit<Event extends E.SoybeanEvent> = Parameters<typeof fetch>["1"] & {
  *     method: "GET",
  *     headers: { "Content-Type": "application/json" },
  *     // Custom callback function
- *     cb: async (e) => {
+ *     cb: async (res, e) => {
  *         // Parse the body
- *         const body = await e.json()
+ *         const body = await res.text()
  *         // Save the body on the event object
  *         // to further process it in the next event handler.
  *         e.set('response', body)
@@ -43,10 +40,10 @@ type FetchInit<Event extends E.SoybeanEvent> = Parameters<typeof fetch>["1"] & {
  * ```
  */
 const $fetch = function<Event extends E.SoybeanEvent = E.SoybeanEvent>(input: FetchInput, init?: FetchInit<Event>): E.EventHandler<Event> {
-    return (e) => new Promise<null | Error>(async end => {
+    return (event) => new Promise<null | Error>(async end => {
         try {
-            const res = await fetch(input, init)
-            if (init && init.cb) await init.cb({ ...res, event: e })
+            const response = await fetch(input, init)
+            if (init && init.cb) await init.cb(response, event)
             end(null)
         }
         catch (error) {
