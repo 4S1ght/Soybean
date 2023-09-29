beebc5fbd7575cd049a6be002b6d682b0326dd2f
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-09-29 20:03:29 +0200

    [add] top-level command "soybean init [file]"

--- src/bin/bin.ts
+++ src/bin/bin.ts
@@ -18,6 +18,41 @@ interface Package {
     description: string
 }
 
+const DEFAULT_CONFIG_FILE = 'soybean.config.js'
+const CONFIG_FILE_NAMES = [ DEFAULT_CONFIG_FILE ]
+
+// ==================================================================
+
+function getConfigLocation(givenLocation: string = ''): [boolean, string, string] {
+
+
+    // Set givenLocation to the default config file name if it is 
+    // an optional parameter, such as --experimental-fetch.
+    if (/--.+/.test(givenLocation)) givenLocation = CONFIG_FILE_NAMES[0]
+
+    // Check whether a file exists
+    const exists = (file: string) => {
+        try   { return (fs.statSync(file)).isFile() } 
+        catch { return false }
+    }
+
+    let isAbs = path.isAbsolute(givenLocation)
+    let bName = path.basename(givenLocation)
+
+    // Default to CWD as the root dir if the path is relative
+    if (!isAbs) givenLocation = path.join(process.cwd(), givenLocation) 
+
+    if (!path.extname(bName)) {
+        for (let i = 0; i < CONFIG_FILE_NAMES.length; i++) {
+            const filename = path.join(givenLocation, CONFIG_FILE_NAMES[i])
+            if (exists(filename)) return [true, filename, path.relative(process.cwd(), filename)]
+        }
+    }
+
+    return [exists(givenLocation), givenLocation, '']
+
+}
+
 // ==================================================================
 
 program
@@ -31,38 +66,7 @@ program
     .allowUnknownOption()
     .action(async (givenLocation: string = '') => {
         
-        const [found, actualLocation, relativeLocation] = (function() {
-
-            const CNF_FILES = [ 'soybean.config.js' ]
-
-            // Set givenLocation to the default config file name if it is 
-            // an optional parameter, such as --experimental-fetch.
-            if (/--.+/.test(givenLocation)) givenLocation = CNF_FILES[0]
-
-            // Check whether a file exists
-            const exists = (file: string) => {
-                try   { return (fs.statSync(file)).isFile() } 
-                catch { return false }
-            }
-        
-            let isAbs = path.isAbsolute(givenLocation)
-            let bName = path.basename(givenLocation)
-
-            // Default to CWD as the root dir if the path is relative
-            if (!isAbs) givenLocation = path.join(process.cwd(), givenLocation) 
-        
-            if (!path.extname(bName)) {
-                for (let i = 0; i < CNF_FILES.length; i++) {
-                    const filename = path.join(givenLocation, CNF_FILES[i])
-                    if (exists(filename)) return [true, filename, path.relative(process.cwd(), filename)]
-                }
-            }
-        
-            console.log(exists(givenLocation), givenLocation, '')
-            return [exists(givenLocation), givenLocation, '']
-
-        })()
-
+        const [found, actualLocation, relativeLocation] = getConfigLocation(givenLocation)
         if (!found) return Terminal.EXIT_HARD(`Could not find the configuration file. Path: "${actualLocation}"`)
 
         Terminal.box(1, 'blue', `Soybean ${c.grey("v"+__package.version)}`)
@@ -76,6 +80,23 @@ program
         if (program instanceof Program === false) Terminal.EXIT_HARD(`Used configuration file must export a program instance created with the "Soybean" function.`)
 
         program.start(cwd)
+
+    })
+
+program
+    .command("init [config-file]")
+    .option('-f, --force', 'Generate a config file forcefully.')
+    .description('Initialize soybean with specified config file')
+    .action(async (givenLocation, flags) => {
+
+        const [found, actualLocation] = getConfigLocation(givenLocation)
+        if (found && !flags.force) return Terminal.EXIT_HARD(`A configuration file already exists in this location. Use -f to force the creation of a new one.`)
+
+        const template = path.join(__dirname, '../../templates/soybean.config.js')
+        const destination = found ? actualLocation : path.join(actualLocation, DEFAULT_CONFIG_FILE)
+        await fs.promises.copyFile(template, destination)
+        
+        Terminal.INFO(`Created a file in "${destination}"`)
 
     })
 
---
+++ templates/soybean.config.js
@@ -0,0 +1,24 @@
+
+import { Soybean } from 'Soybean'
+import h from 'Soybean/handlers'
+
+export default Soybean({
+    cp: {
+        node: {
+            command: ['node'],
+            cwd: './',
+            stdout: 'all'
+        }
+    },
+    routines: {
+        launch: [
+            h.handle(() => console.log("Everything set up!"))
+        ],
+        watch: []
+    },
+    terminal: {
+        passthroughShell: false,
+        keepHistory: false,
+        handlers: {}
+    }
+})
