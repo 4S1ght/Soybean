cc864684fb4954d7d92c07e73bc9ee67c827e6f3
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-11-15 14:46:40 +0100

    [ref] Consolidated package exports into a single object

--- src/exports/index.ts
+++ src/exports/index.ts
@@ -1,4 +1,5 @@
 
+import * as h from '../lib/events/handlers.js'
 import Program from "../lib/program.js"
 import { SoybeanConfig, ZSoybeanConfig } from "../lib/types.js"
 
@@ -6,4 +7,14 @@ export function Soybean(config: SoybeanConfig) {
     ZSoybeanConfig.parse(config)
     process.title = 'Soybean'
     return new Program(config)
+}
+
+export const handlers = {
+    ...h.misc,
+    shell:  h.shell,
+    fs:     h.fs,
+    cp:     h.cp,
+    json:   h.json,
+    net:    h.net,
+    os:     h.os
 }
